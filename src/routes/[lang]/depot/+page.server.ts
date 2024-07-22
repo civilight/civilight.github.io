2c2781d370743f71f7c3e4e3dcf783aa6aa1dbdd
N <42465069+ofcoursenopewastaken@users.noreply.github.com>
2024-07-22 23:42:15 +0700

    Fixed routes

--- src/routes/[lang]/depot/+page.server.ts
+++ src/routes/[lang]/depot/+page.server.ts
@@ -1,7 +1,5 @@
 import { GetDepotData } from "$lib/logic/depot"
 
 export async function load() {
-	console.log("Building depot")
-
 	return GetDepotData()
 }
--- src/routes/[lang]/enemies/+page.server.ts
+++ src/routes/[lang]/enemies/+page.server.ts
@@ -1,7 +1,5 @@
 import { GetEnemiesData } from "$lib/logic/enemies"
 
 export async function load() {
-	console.log("Building enemies")
-
 	return GetEnemiesData()
 }
